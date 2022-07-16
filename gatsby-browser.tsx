d84216701032ac33ab003890b7dcce1a5bb45647
Tony <jeehyukwon@gmail.com>
2022-07-16 21:58:59 +0900

    fix: common css

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,6 +3,7 @@ import "@seed-design/stylesheet/global.css";
 import "@stackflow/basic-ui/index.css"
 import "react-lazy-load-image-component/src/effects/opacity.css";
 import "./src/styles/index.css"
+import "./src/styles/f.css"
 
 import React from "react";
 import { WrapPageElementBrowserArgs } from "gatsby";
